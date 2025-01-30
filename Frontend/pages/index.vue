e06248e6870cae4283fff6bebd339f4ad385babd
AntoDono <anthony20151128@gmail.com>
2025-01-30 01:12:28 -0500

    landing page almost complete

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -1,36 +1,51 @@
 <template>
     <div class="h-screen w-full bg-background1 overflow-hidden">
         <div>
-            <svg width="100%" height="100%">
-                <path d="M0,150 Q300,100 600,150 T1200,150" stroke="red" stroke-width="3" fill="none" />
-            </svg>
-            <div class="text-white">
-                <h1 class="font-playfair text-6xl">There is a Thin Line Between and Hate</h1>
-                <h2 class="font-nunito text-2xl">Does Opposite Attract,</h2>
-                <h2 class="font-nunito text-2xl">Or Are You Going to Get Whacked?</h2>
+            <div ref="strings">
+                <div v-for="index in 20" 
+                     class="fixed top-0 border-2 border-pink-800 w-screen h-screen origin-center spin-slow opacity-20" 
+                     :class="`rotate-${18 * index}`"
+                     :style="{
+                         animationDelay: `${-0.75 * index}s`
+                     }">
+                </div>
+            </div>
+            <div class="fixed w-full h-full flex flex-col justify-center items-center gap-y-10 text-white">
+                <div>
+                    <h1 class="font-playfair text-5xl text-center">There is a Thin Line Between</h1>
+                    <h2 class="font-playfair text-5xl text-center">
+                        Love and Hate
+                    </h2>
+                </div>
+                <div class="flex items-center justify-start flex-col">
+                    <h2 class="font-nunito text-3xl">Does Opposite Attract,</h2>
+                    <h2 class="font-nunito text-3xl">Or Are You Going to Get Whacked?</h2>
+                </div>
             </div>
         </div>
         <div ref="cover" class="fixed top-0 left-0 bg-background2 w-full h-full">
             <div ref="horizontal" class="fixed w-screen">
-                <div class="w-full h-4 bg-primary"></div>
-                <div class="w-full h-4 bg-secondary"></div>
+                <div class="w-full h-8 bg-primary"></div>
+                <div class="w-full h-8 bg-secondary"></div>
             </div>
             <div ref="vertical" class="fixed h-screen flex flex-row">
-                <div class="h-full w-4 bg-primary"></div>
-                <div class="h-full w-4 bg-secondary"></div>
+                <div class="h-full w-8 bg-primary"></div>
+                <div class="h-full w-8 bg-secondary"></div>
             </div>
-            <img ref="bowtie" src="~assets/images/bow.png" class="w-32"/>
+            <img ref="bowtie" src="~assets/images/bow.png" class="w-64"/>
         </div>
     </div>
 </template>
 
 <script setup>
 import gsap from 'gsap';
+import * as PIXI from "pixi.js"
 
 const horizontal = ref(null)
 const vertical = ref(null)
 const bowtie = ref(null)
 const cover = ref(null)
+const canvas = ref(null)
 
 onMounted(async()=>{
     gsap.set(horizontal.value, {
@@ -80,17 +95,30 @@ onMounted(async()=>{
         display: "none"
     })
 
-    const app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight, transparent: true });
-    this.$refs.canvasContainer.appendChild(app.view);
-
-    const graphics = new PIXI.Graphics();
-    graphics.lineStyle(3, 0xff0000, 1);
-    graphics.moveTo(0, 150);
-
-    for (let i = 0; i < window.innerWidth; i += 100) {
-      graphics.quadraticCurveTo(i + 50, Math.random() * 100 + 100, i + 100, 150);
-    }
-
-    app.stage.addChild(graphics);
 })
 </script>
+
+<style>
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+    opacity: 0.7;
+  }
+  50% {
+    transform: rotate(180deg);
+    opacity: 0.15;
+  }
+  100% {
+    transform: rotate(360deg);
+    opacity: 0.7;
+  }
+}
+
+.spin-slow{
+    animation: spin 15s linear infinite;
+}
+
+
+
+</style>
