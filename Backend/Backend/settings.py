30123d329bd8c8e4659ea3494cedd095e59c618d
AntoDono <anthony20151128@gmail.com>
2025-02-04 00:03:26 -0500

    ready for prod

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MODE = os.getenv('DJANGO_MODE', 'development')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -23,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^^3t!)#^j!mnjp*m0383ukh7@+oe2u8sw_9t28kr2o2a*4flg9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if MODE == 'production':
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ["localhost"]
 # Development settings - DO NOT use in production
@@ -124,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
